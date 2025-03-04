bd8bb293ad1d7d41202325c0e6cd6189334e5bca
Richa <gargricha1209@gmail.com>
2025-03-04 11:14:28 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyAuOs66MJDU7JTogKAST7H45wL-9SYXBAM",
+  authDomain: "d-model-app-f373e.firebaseapp.com",
+  projectId: "d-model-app-f373e",
+  storageBucket: "d-model-app-f373e.firebasestorage.app",
+  messagingSenderId: "996878315219",
+  appId: "1:996878315219:web:531c69750b80abf35d2e26"
 };
 
 const app = initializeApp(firebaseConfig);
